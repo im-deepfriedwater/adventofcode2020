1deb29b5120c1b92f1fa4b5a812a33f4bd225c9f
jtorre39 <jtorre39@lion.lmu.edu>
2020-12-15 23:35:40 -0800

    finally day 14...

--- day14/main.py
+++ day14/main.py
@@ -1,4 +1,5 @@
 import re
+import math
 from functools import reduce
 from sys import stdin
 
@@ -24,11 +25,35 @@ for line in [line.rstrip() for line in stdin.readlines()]:
 
 programs.append(program)
 
+def binary_convert(value):
+    return format(int(value), f'036b')
+
+
+def apply_mask(address, mask):
+    padded_address = list(binary_convert(address))
+    for i in range(len(padded_address)):
+        if mask[i] == '1':
+            padded_address[i] = mask[i]
+    return ''.join(padded_address)
+
 for program in programs:
     for instruction in program['instructions']:
-        string = list(format(int(instruction['value']), '036b'))
-        for i, mask_value in enumerate(program['mask']):
-            string[i] = mask_value if mask_value != 'X' else string[i]
-        memory[instruction['address']] = int(''.join(string), 2)
+        addresses = []
+        initial_address = apply_mask(instruction['address'], program['mask'])
+        floating_indices = [i for i, bit in enumerate(program['mask']) if bit == 'X']
+        floating_indices = list(reversed(floating_indices))
+        binary_counter_string = binary_convert(0)
+        while int(binary_counter_string, 2) < math.pow(2, len(floating_indices)):
+            new_address = list(initial_address)
+            for i, floating_index in enumerate(floating_indices):
+                new_address[floating_index] = binary_counter_string[len(binary_counter_string) - i - 1]
+            addresses.append(int(''.join(new_address)))
+            binary_counter_string = binary_convert(int(binary_counter_string, 2) + 1)
 
+        for address in addresses:
+            memory[address] = int(instruction['value'])
 
+# 1350684480495 is too low
+# 4712219970270 is too high ;-;
+
+print(reduce(lambda a, b: a + memory[b], memory.keys(), 0))
